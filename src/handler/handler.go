89a3af1739e5511706eab8b958519f0f0123656b
Sirius <370082192@qq.com>
2020-12-13 16:38:20 +0800

    #文件上传实现，全采用go原生，未使用任何框架

--- src/handler/handler.go
+++ src/handler/handler.go
@@ -4,22 +4,55 @@ package handler
 实现文件的上传和下载
 */
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 //文件上传(这里一定要注意，方法名首字母大写，否则无法在别的包中被引用发现)
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
-	//默认采用post方法提交，所以如果是get则重新返回值文件上传界面
+	fmt.Printf("4")
+	//首次访问指定url默认采用GET方法提交，所以需要调出提交文件表单页面
 	if r.Method == "GET" {
-		data, err := ioutil.ReadFile("static/view/home.html")
+		fmt.Printf("5")
+		//通过读取html文件再交由http.ResponseWriter输出的方式实现文件提交页面的唤出
+		data, err := ioutil.ReadFile("static/view/index.html")
 		if err != nil {
-			io.WriteString(w, "something wrong!")
+			_, _ = io.WriteString(w, "something wrong!")
 			return
 		}
-		io.WriteString(w, string(data))
-	} else if r.Method == "Post" {
+		_, _ = io.WriteString(w, string(data))
+	} else if r.Method == "POST" {
+		fmt.Printf("6")
 		//将文件存储至本地
+		file, head, err := r.FormFile("file")
+
+		if err != nil {
+			fmt.Printf("Failed to get file data %s\n", err.Error())
+			return
+		}
+		defer file.Close()
+		//在本地创建一个新的文件去承载上传的文件
+		newFile, err := os.Create("/tmp/" + head.Filename)
+		if err != nil {
+			fmt.Printf("Failed to create newFile data %s\n", err.Error())
+			return
+		}
+
+		defer newFile.Close()
+		_, err = io.Copy(newFile, file)
+		if err != nil {
+			fmt.Printf("Failed to save into newFile %s\n", err.Error())
+			return
+		}
+		// 重定向到成功的页面逻辑
+		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
 	}
+}
+
+// 文件上传成功处理逻辑
+func UploadSucHandler(w http.ResponseWriter, r *http.Request) {
+	_, _ = io.WriteString(w, "Upload Succeed!")
 }
--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"zone/src/handler"
@@ -8,12 +9,16 @@ import (
 
 func main() {
 	//设置http的路由规则，类似于Java框架中设置请求拦截规则
+	fmt.Printf("1")
 	http.HandleFunc("/file/upload", handler.UploadHandler)
+	fmt.Printf("2")
+	http.HandleFunc("/file/upload/suc", handler.UploadSucHandler)
+	fmt.Printf("3")
 	//开启http监听
 	//err := http.ListenAndServe(":8080", nil)
 	//if err != nil {
 	//	fmt.Printf("There is an err %s", err.Error())
 	//}
 	//上面方法不太优雅，现在用log直接包裹监听
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
