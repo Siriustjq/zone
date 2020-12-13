9cd0885669a79aa50ba5887be3ed668fd3d9aad0
Sirius <370082192@qq.com>
2020-12-13 10:57:49 +0800

    #实现简单的文件上传，开启http端口监听

--- src/handler/handler.go
+++ src/handler/handler.go
@@ -4,16 +4,21 @@ package handler
 实现文件的上传和下载
 */
 import (
-	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 )
 
-//文件上传
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
+//文件上传(这里一定要注意，方法名首字母大写，否则无法在别的包中被引用发现)
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	//默认采用post方法提交，所以如果是get则重新返回值文件上传界面
 	if r.Method == "GET" {
-
+		data, err := ioutil.ReadFile("static/view/home.html")
+		if err != nil {
+			io.WriteString(w, "something wrong!")
+			return
+		}
+		io.WriteString(w, string(data))
 	} else if r.Method == "Post" {
 		//将文件存储至本地
 	}
--- src/main.go
+++ src/main.go
@@ -1,7 +1,17 @@
-package src
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"zone/src/handler"
+)
 
 func main() {
-	fmt.Println("Let's begin!")
+	//设置http的路由规则，类似于Java框架中设置请求拦截规则
+	http.HandleFunc("/file/upload", handler.UploadHandler)
+	//开启http监听
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf("There is an err %s", err.Error())
+	}
 }
