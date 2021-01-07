d9a56fcdde3ebd22c4b4607de811c23d020b9b68
Sirius <370082192@qq.com>
2021-01-07 14:11:28 +0800

    # 完成用户文件信息查询逻辑

--- src/db/userfile.go
+++ src/db/userfile.go
@@ -5,6 +5,16 @@ import (
 	mydb "zone/src/db/mysql"
 )
 
+//构建用户文件结构体，用于用户文件查询的数据返回
+type UserFile struct {
+	UserName    string
+	FileName    string
+	FileHash    string
+	FileSize    int64
+	UploadAt    string
+	LastUpdated string
+}
+
 //UpdateUserFile更新用户文件表
 func UpdateUserFile(username, filename, filehash string, filesize int64) bool {
 	stmt, err := mydb.DBconnect().Prepare(
@@ -20,4 +30,31 @@ func UpdateUserFile(username, filename, filehash string, filesize int64) bool {
 		return false
 	}
 	return true
+}
+
+//QueryUserFile通过username获取指定limit行数的用户文件信息
+func QueryUserFile(username string, limit int64) ([]UserFile, error) {
+	stmt, err := mydb.DBconnect().Prepare("SELECT file_name, file_sha1, file_size, upload_at, last_update " +
+		"FROM tbl_user_file WHERE user_name = ? LIMIT ?")
+	if err != nil {
+		log.Print(SqlConErr)
+		return nil, err
+	}
+	defer stmt.Close()
+	row, err := stmt.Query(username, limit)
+	if err != nil {
+		log.Print(SqlExeErr)
+		return nil, err
+	}
+	var userfiles []UserFile
+	for row.Next() {
+		ufile := UserFile{}
+		err = row.Scan(&ufile.FileName, &ufile.FileHash, &ufile.FileSize, &ufile.UploadAt, &ufile.LastUpdated)
+		if err != nil {
+			log.Print(SqlExeErr)
+			break
+		}
+		userfiles = append(userfiles, ufile)
+	}
+	return userfiles, nil
 }
